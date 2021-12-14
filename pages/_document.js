8d81f8a1c3cbc9e9dbd74fdd656d3afb7add6aea
Umar Nazarov <1nazarovumar@gmail.com>
2021-12-14 18:59:58 +0500

    Document file added

---
+++ pages/_document.js
@@ -0,0 +1,29 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+    static async getInitialProps(ctx) {
+        const initialProps = await Document.getInitialProps(ctx)
+        return { ...initialProps }
+    }
+
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <title>JS Bilim</title>
+                    <link rel="preconnect" href="https://fonts.googleapis.com" />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+                    <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300;400;700&display=swap" rel="stylesheet" />
+                    <meta name="description" content="Js bilim" />
+                    <link rel="icon" href="/favicon.ico" />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
+}
+
+export default MyDocument
