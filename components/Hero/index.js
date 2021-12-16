81701c4c2e82e484241f119ee0a85417965e8e6d
Umar Nazarov <1nazarovumar@gmail.com>
2021-12-16 18:06:51 +0500

    Hero page content changed

--- components/Hero/index.js
+++ components/Hero/index.js
@@ -1,20 +1,24 @@
-import React from 'react'
-import SearchBar from '../SearchBar'
+import Link from 'next/link'
 import cls from './hero.module.scss'
+import Navbar from "../Navbar"
 
 function index() {
     return (
         <div className={cls.container}>
+            <Navbar />
             <div className={cls.content}>
                 <div>
-                    <h1 className={cls._title}>Zamonaviy JavaScript darsligi</h1>
-                    <p className={cls._description}>Endi bu qanday amalga oshirildi. Oddiy, ammo batafsil tushuntirishlar bilan asosiy mavzulardan ilg'or mavzulargacha.</p>
-                    <SearchBar />
+                    <h1 className={cls._title}><span id={cls.underlineded}>Zamonaviy</span> Web dasturlash darsligi</h1>
+                    <p className={cls._description}>Endi bu qanday amalga oshirildi. Oddiy, ammo batafsil tushuntirishlar bilan asosiy mavzulardan ilgor mavzulargacha.</p>
+                    <div className={cls._btn}>
+                        <Link href="/courses">Boshlash (Free)</Link>
+                    </div>
                 </div>
-                <div className={cls.about}>
-                    <h3 className={cls._mainText}>Mundarija</h3>
-                    <p className={cls._mainDescrip}>Asosiy kurs JavaScript-ni dasturlash tili va brauzer bilan ishlashni qamrab oluvchi 2 qismdan iborat. Tematik maqolalarning qo'shimcha turkumlari ham mavjud.</p>
-                </div>
+            </div>
+            <div className={cls.socials}>
+                <a className={cls.social} href='#' blank="true">Instagram</a>
+                <a className={cls.social} href='#' blank="true">Github</a>
+                <a className={cls.social} href='#' blank="true">Facebook</a>
             </div>
         </div>
     )
