cfa54e3146a12154b5248a907ee598390db4142e
Umar Nazarov <1nazarovumar@gmail.com>
2021-12-17 17:49:42 +0500

    courses added with some styles

---
+++ components/Courses/index.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { courses } from '../../mock-data/courses-data'
+
+import cls from './courses.module.scss'
+
+function Courses() {
+    return (
+        <div className={cls.container}>
+            <div className={cls.content}>
+                <h1 className={cls._title}>Mavjud artiklar</h1>
+                <div className={cls.cards}>
+                    {
+                        courses.map(c => (
+                            <div className={cls._card}>
+                                <div>
+                                    <img className={cls._image} src={c.image} />
+                                    <h1 className={cls._name}>{c.title}</h1>
+                                    <p className={cls._author}>Muallif: {c.author}</p>
+                                    <p className={cls._description}>{c.description}</p>
+                                    <button className={cls._btn}>Boshlash</button>
+                                </div>
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Courses
