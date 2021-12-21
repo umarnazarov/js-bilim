df32780cc09809d95ce870ad6732383dd5246c73
Umar Nazarov <1nazarovumar@gmail.com>
2021-12-21 14:26:07 +0500

    courses minor changes

--- components/Card/index.js
+++ components/Card/index.js
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 import { courses } from '../../mock-data/courses-data'
 import cls from './card.module.scss'
@@ -9,11 +10,10 @@ function index() {
                 courses.map(c => (
                     <div key={c.title} className={cls._card}>
                         <div>
-                            {/* <img className={cls._image} src={c.image} /> */}
                             <h1 className={cls._name}>{c.title}</h1>
                             <p className={cls._author}>Muallif: {c.author}</p>
                             <p className={cls._description}>{c.description}</p>
-                            <button className={cls._btn}>Boshlash</button>
+                            <Link href={c.link} className={cls._btn}>Boshlash</Link>
                         </div>
                     </div>
                 ))
--- mock-data/courses-data.js
+++ mock-data/courses-data.js
@@ -1,46 +1,40 @@
-import js from '../images/js.png'
-import html5 from '../images/html5.png'
-import node from '../images/node.png'
-import css from '../images/css.png'
-import redux from '../images/redux.png'
-
 
 
 export const courses = [
     {
-        image: html5.src,
         title: 'HTML5',
         description: "HTML5 kursi from zero to hero ga aylaning ",
-        author: "Leo Messi"
+        author: "Mark Zuckerbur",
+        link: '/getting-started'
     },
     {
-        image: css.src,
         title: 'CSS3',
         description: "CSS3 kursi from zero to hero ga aylaning ",
-        author: "Selena Gomez"
+        author: "Mark Zuckerbur",
+        link: '/getting-started'
     },
     {
-        image: js.src,
         title: 'Javascript',
         description: "Javascript kursi from zero to hero ga aylaning ",
-        author: "Mark Zuckerbur"
+        author: "Mark Zuckerbur",
+        link: '/getting-started'
     },
     {
-        image: redux.src,
         title: 'React JS',
         description: "React JS kursi from zero to hero ga aylaning ",
-        author: "Umar Nazarov"
+        author: "Umar Nazarov",
+        link: '/use-state'
     },
     {
-        image: node.src,
         title: 'Node JS',
         description: "Node JS kursi from zero to hero ga aylaning ",
-        author: "Iron Man"
+        author: "Iron Man",
+        link: '/getting-started'
     },
     {
-        image: redux.src,
         title: 'Redux',
         description: "Redux kursi from zero to hero ga aylaning ",
-        author: "Ilon Mask"
+        author: "Ilon Mask",
+        link: '/getting-started'
     }
 ]
