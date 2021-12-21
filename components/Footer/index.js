ff6d8a246b8f17e1a6815a27e619763c916394aa
Umar Nazarov <1nazarovumar@gmail.com>
2021-12-21 10:07:56 +0500

    footer added

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,11 +1,13 @@
+import cls from "./footer.module.scss"
 
 function index() {
     return (
-        <div>
-            <div>
-                <ul>
-                    <li>Proekt haqida</li>
-                    <li>Biz bilan boglanish</li>
+        <div className={cls.container}>
+            <div className={cls.content}>
+                <ul className={cls._links}>
+                    <li className={cls._link}>2021 Umar</li>
+                    <li className={cls._link}>Proekt haqida</li>
+                    <li className={cls._link}>Biz bilan boglanish</li>
                 </ul>
             </div>
         </div>
