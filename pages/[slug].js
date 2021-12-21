1262c20bd4d5af951d6a780c6765b1bcfcce1224
Umar Nazarov <1nazarovumar@gmail.com>
2021-12-21 10:08:20 +0500

    right side content added in slug

--- pages/[slug].js
+++ pages/[slug].js
@@ -1,13 +1,13 @@
-import { serialize } from 'next-mdx-remote/serialize'
+import { serialize } from 'next-mdx-remote/serialize';
 import glob from 'glob';
-import { MDXRemote } from 'next-mdx-remote'
-import fs from 'fs'
-import path from 'path'
-import matter from 'gray-matter'
-import SyntaxHighlighter from 'react-syntax-highlighter'
+import { MDXRemote } from 'next-mdx-remote';
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+import SyntaxHighlighter from 'react-syntax-highlighter';
 
 
-const PostPage = ({ frontMatter: { title }, mdxSource }) => {
+const PostPage = ({ frontMatter: { title }, mdxSource, filterSections }) => {
     return (
         <div className="mt-4">
             <h1>{title}</h1>
@@ -18,9 +18,12 @@ const PostPage = ({ frontMatter: { title }, mdxSource }) => {
 
 const getStaticPaths = async () => {
 
-    const allFiles = glob.sync('tutorials/**/*.mdx')
+    const allFiles = glob.sync('tutorials/**/*.mdx');
 
-    const slugs = allFiles.map(filename => filename.split("/")).map(f => f[f.length - 1].replace('.mdx', ''))
+    const slugs = allFiles.map(filename => filename
+        .split("/"))
+        .map(f => f[f.length - 1]
+            .replace('.mdx', ''));
 
     const paths = slugs.map(slug => ({
         params: {
@@ -38,22 +41,51 @@ const getStaticPaths = async () => {
 }
 
 const getStaticProps = async ({ params: { slug } }) => {
-    const allFiles = glob.sync('tutorials/**/*.mdx')
-    let existPath = allFiles.find(f => f.includes(slug))
 
-    const markdownWithMeta = fs.readFileSync(path.join(existPath), 'utf-8')
+    const allFiles = glob.sync('tutorials/**/*.mdx');
+    let existPath = allFiles.find(f => f.includes(slug));
 
-    const { data: frontMatter, content } = matter(markdownWithMeta)
-    const mdxSource = await serialize(content)
+    // Getting right side content
+    let tutorials = existPath.split('/');
+    let sections = allFiles.filter(f => f.includes(tutorials[1]));
+
+    let grupedSectionsWithChilds = sections.reduce((prev, val) => {
+        const section = new Object;
+        let folderName = val.split('/')[2]
+        section.title = folderName;
+        section.childs = sections
+            .filter(c => c
+                .includes(folderName))
+            .map(c => c
+                .replace(/[/]/g, " ")
+                .replace('.mdx', '')
+                .split(' ')[3])
+        return [...prev, section];
+    }, []);
+
+
+
+    let filterSections = grupedSectionsWithChilds.filter((value, index, self) =>
+        index === self.findIndex((t) => (
+            t.title === value.title
+        ))
+    );
+    console.log(filterSections)
+
+    const markdownWithMeta = fs.readFileSync(path.join(existPath), 'utf-8');
+
+    const { data: frontMatter, content } = matter(markdownWithMeta);
+    const mdxSource = await serialize(content);
 
     return {
         props: {
             frontMatter,
             slug,
-            mdxSource
+            mdxSource,
+            filterSections
         }
     }
 }
 
 export { getStaticProps, getStaticPaths }
-export default PostPage
+export default PostPage;
