302543c44e0bab2c7e9e3bf036ef68230171ac37
Umar Nazarov <1nazarovumar@gmail.com>
2021-12-21 18:17:13 +0500

    left side content added

--- components/LeftSideContent/index.js
+++ components/LeftSideContent/index.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import Link from 'next/link'
 import cls from './left_side_content.module.scss'
 
-function index() {
+function index({ content }) {
     return (
         <div className={cls.container}>
             <div className={cls.content}>
-                Article Content should be here
+                <p className={cls._title}>Ushbu Artiklda</p>
+                <ul className={cls._links}>
+                    {content.map(c => (
+                        <li className={cls._link}>
+                            <Link href={"#" + c}>{c.replace('-', ' ')}</Link>
+                        </li>
+                    ))}
+                </ul>
             </div>
         </div>
     )
